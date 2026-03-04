cb8c34ef740d331f79197bf237b4b71d6b5bdb75
Dennis Zacharia <dennyzach15@gmail.com>
2026-03-04 10:50:32 -0500

    Update banking.py

--- banking.py
+++ banking.py
@@ -1,1 +1,38 @@
+from datetime import datetime
 
+
+class Transaction:
+    def __init__(self, amount, timestamp=None):
+        self.amount = float(amount)
+
+        if timestamp is None:
+            self.timestamp = datetime.now()
+        else:
+            self.timestamp = timestamp
+
+    def __repr__(self):
+        return f"Transaction(amount={self.amount}, timestamp={repr(self.timestamp)})"
+
+    def __str__(self):
+        sign = "+" if self.amount >= 0 else "-"
+        amount = abs(self.amount)
+        date = self.timestamp.date()
+        return f"{date}: {sign}${amount:,.2f}"
+
+
+class Account:
+    def __init__(self):
+        self.transactions = []
+
+    def deposit(self, amount):
+        amount = abs(amount)
+        transaction = Transaction(amount)
+        self.transactions.append(transaction)
+
+    def withdraw(self, amount):
+        amount = -abs(amount)
+        transaction = Transaction(amount)
+        self.transactions.append(transaction)
+
+    def get_balance(self):
+        return sum(t.amount for t in self.transactions)
